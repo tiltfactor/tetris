f5683054f7c493ae8a26066906df7007220ad208
NikoruChen <shiland@163.com>
2016-11-08 17:56:12 -0500

    Better timer. Need to format position of timer

--- tetris.js
+++ tetris.js
@@ -20,13 +20,44 @@
 // IN THE SOFTWARE.
 // ============================================================================
 
+//countdown timer
+
+var gameTimeOut = 300;
+
+function countdown(minutes) {
+    var seconds = 60;
+    var mins = minutes;
+    function tick() {
+      gameTimeOut -= 1;
+      console.log (gameTimeOut);
+        var counter = document.getElementById("timer");
+        var current_minutes = mins-1
+        seconds--;
+        counter.innerHTML =
+        current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+        if( seconds > 0 ) {
+            setTimeout(tick, 1000);
+        } else {
+ 
+            if(mins > 1){
+ 
+               // countdown(mins-1);   never reach “00″ issue solved:Contributed by Victor Streithorst
+               setTimeout(function () { 
+                countdown(mins - 1); 
+              }, 1000);
+ 
+            }
+        }
+    }
+    tick();
+}
+
 var TETRIS = new function () { // namespacing
 
-	var startTime = new Date().getTime();
-	console.log(startTime);
-	var gameTimeOut = 300000; 
+	// var startTime = new Date().getTime();
+	// console.log(startTime);
+	// var gameTimeOut = 300000; 
   var gameIsOver = false;
-
 
 function random_det(seed) {
   return function() {
@@ -693,8 +724,10 @@ function kick() {
 }
 
 function updatePiece() {
-  var timeNow = new Date().getTime();
-  if (timeNow - startTime > gameTimeOut) {
+  // var timeNow = new Date().getTime();
+  // if (timeNow - startTime > gameTimeOut) {
+  // console.log (gameTimeOut);
+  if (gameTimeOut < 0) {
     gameOver();
   }
   var ctx = document.getElementById('animated_canvas').getContext('2d');
