eb24fc8ccd1113d482b4e26b4fb9c783adfcf35a
Max Seidman <max@tiltfactor.org>
2016-11-30 11:00:01 -0500

    Rework statistics parser to compile in a single line csv for each player
    Includes adding start time, game duration, line clears, and userID to the csv.
    Stats that reviewed multiple board states had to be dealt with to return
    a single number instead of an array.  Highest piece and Avg board height
    now return the MAX of all of the board states.  I disabled count cells and
    count holes because theoretically line clears will deal with that.

--- parsing/parsers/exportStats.js
+++ parsing/parsers/exportStats.js
@@ -38,22 +38,33 @@ function chooseFunctions(form) {
 
 function getStats(functions) {
 	var sessions = JSON.parse(sessionStorage.getItem("data"));
+	var headings = functions.slice(0);
+	headings.push("lineClears","userID", "duration (seconds)","start time");
+	var data = {
+		id: "data",
+		boardStats: [headings]
+	};
 	for (var i = 0, sessionsLength = sessions.length; i < sessionsLength; i++) {
 		var boards = getBoards(sessions[i]);
-		var data = {
-			id: sessions[i].id,
-			boardStats: [functions]
-		};
+		stats = Array.apply(null, Array(functions.length)).map(Number.prototype.valueOf,0);
 		for (var j = 0, boardsLength = boards.length; j < boardsLength; j++) {
 			board = boards[j];
-			stats = [];
 			for (var k = 0, funcsLength = functions.length; k < funcsLength; k++) {
-				stats.push(window[functions[k]](board));
+				var result = window[functions[k]](board);
+				if(result > stats[k]){
+					stats[k] = result;
+				}
 			}
-			data.boardStats.push(stats);
+			if(j == 0){
+				stats.push(getLineClears(sessions[i]));
+				stats.push(getID(sessions[i]));
+				stats.push(getDuration(sessions[i]));
+				stats.push(getStartTime(sessions[i]));
+			}
 		}
-		showDownload(data);
+		data.boardStats.push(stats);
 	}
+	showDownload(data);
 }
 
 function getBoards(session) {
@@ -65,6 +76,55 @@ function getBoards(session) {
 		}
 	}
 	return boards;
+}
+
+function getLineClears(session) {
+	var gameEvents = session.events;
+	var clears = 0;
+	for (var i = 0, eventsLength = gameEvents.length; i < eventsLength; i++) {
+		if (gameEvents[i].type == "clear") {
+			clears += gameEvents[i].details;
+		}
+	}
+	return clears;
+}
+
+function getDuration(session) {
+	var gameEvents = session.events;
+	var startTime = 0;
+	var duration = 0;
+	for (var i = 0, eventsLength = gameEvents.length; i < eventsLength; i++) {
+		if (gameEvents[i].type == "start") {
+			startTime = gameEvents[i].timestamp;
+		}
+		if (gameEvents[i].type == "quit") {
+			duration = (gameEvents[i].timestamp - startTime)/1000;
+		}
+	}
+	return duration;
+}
+
+function getStartTime(session) {
+	var gameEvents = session.events;
+	var startTime = 0;
+	for (var i = 0, eventsLength = gameEvents.length; i < eventsLength; i++) {
+		if (gameEvents[i].type == "start") {
+			startTime = gameEvents[i].timestamp;
+			return startTime;
+		}
+	}
+	return startTime;
+}
+
+function getID(session) {
+	var gameEvents = session.events;
+	var id = "NO ID FOUND";
+	for (var i = 0, eventsLength = gameEvents.length; i < eventsLength; i++) {
+		if (gameEvents[i].type == "qid") {
+			id = gameEvents[i].details;
+		}
+	}
+	return id;
 }
 
 function showDownload(data) {
